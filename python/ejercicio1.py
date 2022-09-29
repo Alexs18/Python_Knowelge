7127da0872bd3a5fad43bfb2c6f2285d495b7aa2
alex lopez <mclexis18@gmail.com>
2022-09-28 22:13:16 -0500

    Alcance de los datos y creacion de variables

--- python/ejercicio1.py
+++ python/ejercicio1.py
@@ -1,5 +1,25 @@
+#Impresiones en pantalla con pythonn
 print('hello world')
 a = 'a'
 b ='b'
 c = 'c'
 print('valor a  {}, valor b {} valor c {}' .format(a,b,c))
+
+#Alcnce de los datos en python
+
+#Alcance de las variables: como no declaramos variables en python, el valor puede
+#Cambiar simpre que a esa variable que se le asigne un valor y esta pueda mostrarce
+a = 'alexis'
+print(a)
+
+a= 'Ricardo'
+print(a)
+
+a = 'Lopez'
+print(a)
+
+#Delcaracion de variables
+
+a, b, c = 'Tuarez', 23, 'Nicolas'
+
+print('su nombre es {}, edad: {}, apellido: {}'. format(c, b,a))
