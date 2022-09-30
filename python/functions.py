0d1589be7359a5b40fc5c1264b08100eeda7607e
alex lopez <mclexis18@gmail.com>
2022-09-29 21:31:12 -0500

    Funciones en python

---
+++ python/functions.py
@@ -0,0 +1,26 @@
+#Funciones en python
+
+#this is the way to make a function en python
+#Function withput params
+def sayhello():
+    print('hola one again')
+
+#Call the function
+sayhello()
+
+#function with params
+
+def sumall(a, b):
+    print(a+b)
+
+sumall(1,2)
+    
+#function with conditions
+
+def condition(a, b):
+    if a > b:
+        return 'true'
+    else:
+        return 'false'
+
+condition(5,4)
